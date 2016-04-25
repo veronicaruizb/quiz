ab2971b9eeb6a90b93c83e79cf5d785b0c17ceb9
veronicaruizb <veronica.ruiz.bejerano@alumnos.upm.es>
2016-04-25 18:47:05 +0200

    No perder respuesta

---
+++ controllers/quiz_controller.js
@@ -0,0 +1,14 @@
+//GET /question
+exports.question = function(req, res, next) {
+	var answer = req.query.answer || '';
+	res.render('quizzes/question', {question: 'Capital de Italia', answer: answer});
+};
+
+//GET /check
+exports.check = function(req, res, next){
+
+	var answer = req.query.answer || "";
+
+	var result = req.query.answer === 'Roma' ? 'Correcta' : 'Incorrecta';
+	res.render('quizzes/result', {result: result, answer: answer});
+};
