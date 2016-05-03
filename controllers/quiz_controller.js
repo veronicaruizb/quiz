91313432ff1f0cd5752a64770d85638b1fbb2cb0
veronicaruizb <veronica.ruiz.bejerano@alumnos.upm.es>
2016-05-03 12:43:15 +0200

    Base de Datos

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,14 +1,28 @@
+var models = require('../models');
+
 //GET /question
 exports.question = function(req, res, next) {
-	var answer = req.query.answer || '';
-	res.render('quizzes/question', {question: 'Capital de Italia', answer: answer});
+	models.Quiz.findOne().then(function(quiz){
+		if(quiz){
+			var answer = req.query.answer || '';
+			res.render('quizzes/question', {question: 'Capital de Italia', answer: answer});
+		}
+		else {
+			throw new Error('No hay preguntas en la BBDD.');
+		}
+	}).catch(function(error){next(error);});
 };
 
 //GET /check
 exports.check = function(req, res, next){
-
-	var answer = req.query.answer || "";
-
-	var result = req.query.answer === 'Roma' ? 'Correcta' : 'Incorrecta';
-	res.render('quizzes/result', {result: result, answer: answer});
+	models.Quiz.findOne().then(function(quiz){
+		if(quiz){
+			var answer = req.query.answer || "";
+			var result = req.query.answer === 'Roma' ? 'Correcta' : 'Incorrecta';
+			res.render('quizzes/result', {result: result, answer: answer});
+		}
+		else{
+			throw new Error('No hay preguntas en la BBDD.');
+		}
+	}).catch(function(error){next(error);});
 };
---
+++ models/quiz.js
@@ -0,0 +1,8 @@
+// Definicion del modelo Quiz:
+
+module.exports = function(sequelize, DataTypes) {
+	return sequelize.define('Quiz', 
+		{ question: DataTypes.STRING,
+		  answer: DataTypes.STRING
+		});
+};
