c4eec093df197ab22063f72d362f47801e0fa8bd
veronicaruizb <veronica.ruiz.bejerano@alumnos.upm.es>
2016-05-24 12:40:57 +0200

    Crear Comentarios

--- models/index.js
+++ models/index.js
@@ -20,7 +20,13 @@ var sequelize = new Sequelize (url, {
 });
 
 // Importar la definicion de la tabla Quiz de quiz.js
-
 var Quiz = sequelize.import(path.join(__dirname,'quiz'));
+// Importar la definicion de la tabla COmmments de comment.js
+var Comment = sequelize.import(path.join(__dirname, 'comment'));
+
+//Relaciones entre modelos
+Comment.belongsTo(Quiz);
+Quiz.hasMany(Comment);
 
 exports.Quiz = Quiz;
+exports.Comment = Comment;
