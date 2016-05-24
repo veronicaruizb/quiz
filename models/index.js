69bf019a9f8f6101c314e2d487612eee82d46a73
veronicaruizb <veronica.ruiz.bejerano@alumnos.upm.es>
2016-05-24 11:58:29 +0200

    Migraciones y Seeders

--- models/index.js
+++ models/index.js
@@ -23,22 +23,4 @@ var sequelize = new Sequelize (url, {
 
 var Quiz = sequelize.import(path.join(__dirname,'quiz'));
 
-//sequelize.sync() crea e inicializa tabla de preguntas en DB
-
-sequelize.sync().then(function(){
-	return Quiz.count().then(function(c){
-		if (c === 0){
-			return Quiz.bulkCreate(
-				[ {question: 'Capital de Italia', answer: 'Roma'},
-				  {question: 'Capital de Portugal', answer: 'Lisboa'}
-				]).then(function(){
-					console.log('Base de datos inicializada con datos');
-				});
-		}
-	});
-}).catch(function(error){
-	console.log("Error sincronizando las tablas de la BBDD:", error);
-	process.exit(1);
-});
-
 exports.Quiz = Quiz;
