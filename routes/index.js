ddfd0fecc3c4180307c70d9bc4082004cc0e019e
veronicaruizb <veronica.ruiz.bejerano@alumnos.upm.es>
2016-05-25 11:48:12 +0200

    Gestion de Usuarios

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 var quizController = require('../controllers/quiz_controller');
 var commentController = require('../controllers/comment_controller');
-
+var userController = require('../controllers/user_controller');
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index');
@@ -14,6 +14,7 @@ router.get('/author', function(req,res,next){
 
 //autoload de rutas que usen :quizId
 router.param('quizId', quizController.load);
+router.param('userId', userController.load);
 
 
 //Definicion de rutas de /quizzes
@@ -28,5 +29,15 @@ router.delete('/quizzes/:quizId(\\d+)', quizController.destroy);
 
 router.get('/quizzes/:quizId(\\d+)/comments/new', commentController.new);
 router.post('/quizzes/:quizId(\\d+)/comments', commentController.create);
+
+//Definición de rutas de cuenta
+router.get('/users', userController.index);
+router.get('/users/:userId(\\d+)', userController.show);
+router.get('/users/new', userController.new);
+router.post('/users', userController.create);
+router.get('/users/:userId(\\d+)/edit', userController.edit);
+router.put('/users/:userId(\\d+)', userController.update);
+router.delete('/users/:userId(\\d+)', userController.destroy);
+
 
 module.exports = router;
