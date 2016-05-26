532fd43aeb644fa70a4e2e189b0c714cc120e50c
veronicaruizb <veronica.ruiz.bejerano@alumnos.upm.es>
2016-05-26 11:09:04 +0200

    Roles y permisos

--- controllers/session_controller.js
+++ controllers/session_controller.js
@@ -29,7 +29,7 @@ exports.create = function (req, res, next){
 		if(user){
 			var ta = new Date();
 			var tb = ta.getTime();
-			req.session.user = {id:user.id, username:user.username};
+			req.session.user = {id:user.id, username:user.username, isAdmin:user.isAdmin};
 			res.redirect(redir);
 		}else{
 			req.flash('error', 'La autentificación ha fallado. Reinténtelo otra vez');
@@ -53,5 +53,41 @@ exports.loginRequired = function(req, res, next){
 		next();
 	} else {
 		res.redirect('/session?redir=' + (req.param('redir')||req.url));
+	}
+};
+
+exports.adminRequired = function(req, res, next){
+	var isAdmin = req.session.user.isAdmin;
+	if(isAdmin){
+		next();
+	} else {
+		console.log('Ruta prohibida: el usuario logeado no es administrador.');
+		res.send(403);
+	}
+};
+
+
+exports.adminOrMyselfRequired = function(req, res, next){
+	var isAdmin = req.session.user.isAdmin;
+	var userId = req.user.id;
+	var loggedUserId = req.session.user.id;
+
+	if(isAdmin || userId === loggedUserId){
+		next();
+	} else {
+		console.log('Ruta prohibida: no es el usuario logeado, ni un administrador.');
+		res.send(403);
+	}
+};
+
+exports.adminAndNotMyselfRequired = function(req, res, next){
+	var isAdmin = req.session.user.isAdmin;
+	var userId = req.user.id;
+	var loggedUserId = req.session.user.id;
+	if(isAdmin || userId === loggedUserId){
+		next();
+	}else{
+		console.log('Ruta prohibida: no es el usuario logeado, ni un administrador.');
+		res.send(403); 
 	}
 };
--- routes/index.js
+++ routes/index.js
@@ -26,22 +26,22 @@ router.get('/quizzes/:quizId(\\d+).:format?', quizController.show);
 router.get('/quizzes/:quizId(\\d+)/check', quizController.check);
 router.get('/quizzes/new', sessionController.loginRequired, quizController.new);
 router.post('/quizzes', sessionController.loginRequired, quizController.create);
-router.get('/quizzes/:quizId(\\d+)/edit', sessionController.loginRequired, quizController.edit);
-router.put('/quizzes/:quizId(\\d+)', sessionController.loginRequired, quizController.update);
-router.delete('/quizzes/:quizId(\\d+)', sessionController.loginRequired, quizController.destroy);
+router.get('/quizzes/:quizId(\\d+)/edit', sessionController.loginRequired, quizController.ownershipRequired, quizController.edit);
+router.put('/quizzes/:quizId(\\d+)', sessionController.loginRequired, quizController.ownershipRequired, quizController.update);
+router.delete('/quizzes/:quizId(\\d+)', sessionController.loginRequired, quizController.ownershipRequired, quizController.destroy);
 
 router.get('/quizzes/:quizId(\\d+)/comments/new', sessionController.loginRequired, commentController.new);
 router.post('/quizzes/:quizId(\\d+)/comments', sessionController.loginRequired, commentController.create);
-router.put('/quizzes/:quizId(\\d+)/comments/:commentId(\\d+)/accept', sessionController.loginRequired, commentController.accept);
+router.put('/quizzes/:quizId(\\d+)/comments/:commentId(\\d+)/accept', sessionController.loginRequired, quizController.ownershipRequired, commentController.accept);
 
 //Definición de rutas de cuenta
 router.get('/users', userController.index);
 router.get('/users/:userId(\\d+)', userController.show);
 router.get('/users/new', userController.new);
 router.post('/users', userController.create);
-router.get('/users/:userId(\\d+)/edit', sessionController.loginRequired, userController.edit);
-router.put('/users/:userId(\\d+)', sessionController.loginRequired, userController.update);
-router.delete('/users/:userId(\\d+)', sessionController.loginRequired, userController.destroy);
+router.get('/users/:userId(\\d+)/edit', sessionController.loginRequired, sessionController.adminOrMyselfRequired, userController.edit);
+router.put('/users/:userId(\\d+)', sessionController.loginRequired, sessionController.adminOrMyselfRequired, userController.update);
+router.delete('/users/:userId(\\d+)', sessionController.loginRequired, sessionController.adminAndNotMyselfRequired, userController.destroy);
 
 //Definición de rutas de sesion
 router.get('/session', sessionController.new);
